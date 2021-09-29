786d64820eb3b84e8a676edeebea175e065576be
Tekla Tóth <tekla952@gmail.com>
2021-09-29 11:38:36 +0200

    Add reader functions for ellipse fitting in stereo images

--- LCS_ellipse.m
+++ LCS_ellipse.m
@@ -1,4 +1,32 @@
 function []  = LCS_ellipse()
+%% detect ellipses in stereo images
+%%
+% read images
+load('input.mat',...
+    'sphImagePath1', 'sphImagePath2', 'sphImageNames');
+[imgs1, imgs2] = readStereoImgs(sphImagePath1, sphImageNames, sphImagePath2, sphImageNames);
+numImgs = length(imgs1);
+ellipses1 = zeros(numImgs, 5);
+ellipses2 = zeros(numImgs, 5);
+
+%%
+%detect ellipses
+for i = 1 : numImgs
+    disp('--------------------------------------------------------------');
+    disp(i);
+    disp('--------------------------------------------------------------');
+    ellipseTmp = LCS_ellipse_from_img(imgs1{i});
+    ellipses1(i, :) = ellipseTmp(1, :);
+    ellipseTmp = LCS_ellipse_from_img(imgs2{i});
+    ellipses2(i, :) = ellipseTmp(1, :);
+end
+
+%%
+% save result
+save('output.mat', 'ellipses1', 'ellipses2');
+end
+
+function [ellipses]  = LCS_ellipse_from_img(I)
 %% parameters illustration
 %1) Tac: 
 %The threshold of elliptic angular coverage which ranges from 0~360. 
@@ -11,11 +39,7 @@ function []  = LCS_ellipse()
 %-1 means detecting the ellipses with negative polarity; 
 %0 means detecting all ellipses from image
 
-
 close all;
-
-%image path
-filename = 'D:\Graduate Design\Ellipse Detection\MyEllipse - github\pics\666.jpg';
 
 % parameters
 Tac = 165;
@@ -24,8 +48,11 @@ specified_polarity = 0;
 
 %%
 % read image 
-disp('------read image------');
-I = imread(filename);
+%image path
+%filename = 'C:\Users\totht\ELTE\SphereCalib\examples\example4\Scene2\Sphere\2D1\(1).jpg'
+%'C:\Users\totht\ELTE\High-quality-ellipse-detection\pics\666.jpg';
+%disp('------read image------');
+%I = imread(filename);
 
 
 %% detecting ellipses from real-world images
@@ -35,7 +62,7 @@ disp('draw detected ellipses');
 drawEllipses(ellipses',I);
 % display
 ellipses(:,5) = ellipses(:,5)./pi*180;
-ellipses
+disp(ellipses);
 disp(['The total number of detected ellipses£º',num2str(size(ellipses,1))]);
 
 %% draw ellipse centers
@@ -46,6 +73,55 @@ disp(['The total number of detected ellipses£º',num2str(size(ellipses,1))]);
 %% write the result image
 %set(gcf,'position',[0 0 size(I,2) size(I,1)]);
 %saveas(gcf, 'D:\Graduate Design\Ellipse Detection\MyEllipse - github\pics\666_all.jpg', 'jpg');
+end
+
+function [imgs1, imgs2] = readStereoImgs(imgDir1, imgNames1, imgDir2, imgNames2)
+%
+% Read stereo image
+%
+% 
+% INPUT
+% Source file infos
+% - imgDir1, imgNames1 -- first image path and file names
+% - imgDir2, imgNames2 -- second image path and file names
+%
+%
+numImgs1 = length(imgNames1);
+if numImgs1 < 1
+    error('Not enough input image file names (second argument).');
+end
+
+numImgs2 = length(imgNames2);
+if numImgs2 < 1
+    error('Not enough input image file names (fourth argument).');
+end
+
+if numImgs1 ~= numImgs2
+    error('Number of input stereo images are different (second and fourth argument).');
+end
+
+imgs1 = readImgs(imgDir1, imgNames1);
+imgs2 = readImgs(imgDir2, imgNames2);
+
+end
+
+function [imgs] = readImgs(imgDir, imgNames)
+%
+% Read image
+%
+% 
+% INPUT
+% Source file infos
+% - imgDir, imgNames -- image path and file names
+%
+%
+imgs = cell(length(imgNames), 1);
+
+for i= 1:length(imgNames)
+    imgFile = strcat(imgDir,imgNames(i));
+    imgs{i} = imread(char(imgFile));
+end
+
 end
 
 
